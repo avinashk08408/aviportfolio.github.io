3986b14539959a0c65805e05aca15a1dcd562243
avinashk08408 <avinashk08408@gmail.com>
2026-07-24 22:31:47 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Portfolio is under development...</p>
+    </div>
+  );
+}
+
+export default App;
