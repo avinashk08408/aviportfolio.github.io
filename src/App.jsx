2c6606be0e2d820ad98d5d7b7f04be5fba44d4bd
avinashk08408 <avinashk08408@gmail.com>
2026-07-24 22:53:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Welcome to My Portfolio</h1>
-      <p>Portfolio is under development...</p>
-    </div>
+    <>
+      <Navbar />
+
+      <main>
+        <section
+          id="home"
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "3rem",
+            fontWeight: "700",
+          }}
+        >
+          Home
+        </section>
+
+        <section id="about" style={{ height: "100vh" }}></section>
+
+        <section id="skills" style={{ height: "100vh" }}></section>
+
+        <section id="projects" style={{ height: "100vh" }}></section>
+
+        <section id="experience" style={{ height: "100vh" }}></section>
+
+        <section id="contact" style={{ height: "100vh" }}></section>
+      </main>
+    </>
   );
 }
 
