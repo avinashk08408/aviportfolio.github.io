32e18560c2a0e685c9bcae2c2e756724b1d3bcae
avinashk08408 <avinashk08408@gmail.com>
2026-07-25 12:23:18 +0530

    Add Contact component to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Experience from "./components/Experience/Experience";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <Experience />
+      <Contact />
     </>
   );
 }
