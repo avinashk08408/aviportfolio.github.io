47f9413b0b4e5fad7b39938fa51f7ebad0785978
avinashk08408 <avinashk08408@gmail.com>
2026-07-25 09:51:23 +0530

    Replace main sections with Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <main>
-        <section
-          id="home"
-          style={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            fontSize: "3rem",
-            fontWeight: "700",
-          }}
-        >
-          Home
-        </section>
-
-        <section id="about" style={{ height: "100vh" }}></section>
-
-        <section id="skills" style={{ height: "100vh" }}></section>
-
-        <section id="projects" style={{ height: "100vh" }}></section>
-
-        <section id="experience" style={{ height: "100vh" }}></section>
-
-        <section id="contact" style={{ height: "100vh" }}></section>
-      </main>
+      <Hero />
     </>
   );
 }
