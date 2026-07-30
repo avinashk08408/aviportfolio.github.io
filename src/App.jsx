72c20b09e95a785f1c105f5c340858f6d6a22133
avinashk08408 <avinashk08408@gmail.com>
2026-07-30 21:53:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
-import Experience from "./components/Experience/Experience";
+import Journey from "./components/Journey/Journey";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
@@ -25,8 +25,7 @@ function App() {
 
         <Projects />
 
-        <Experience />
-
+        <Journey />
         <Contact />
 
       </main>
