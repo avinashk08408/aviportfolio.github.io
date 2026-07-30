9d610ccfc2d6eb362db6247ddee1fa673028a629
avinashk08408 <avinashk08408@gmail.com>
2026-07-30 20:32:46 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,42 +5,43 @@ function About() {
   return (
     <section className="about" id="about">
 
-      <div className="section-heading">
+      <div className="section-title">
         <p>PROFILE</p>
         <h2>Who I Am</h2>
       </div>
 
-      <div className="about-container">
+      <div className="about-wrapper">
 
-        <div className="about-image">
+        <div className="about-left">
           <img src={profile} alt="Avinash" />
         </div>
 
-        <div className="about-content">
+        <div className="about-right">
 
           <h3>Cyber Security Student & Web Developer</h3>
 
           <p>
             I'm <strong>Avinash</strong>, a Cyber Security student at
-            <strong> SRM Valliammai Engineering College</strong>.
-            I enjoy building secure web applications, exploring ethical hacking,
-            and creating modern digital experiences using React, Python and
-            cybersecurity tools.
+            <strong> SRM Valliammai Engineering College</strong> with a passion
+            for building secure web applications and learning ethical hacking,
+            networking, and modern web technologies. I enjoy turning ideas into
+            practical, user-friendly solutions while continuously improving my
+            cybersecurity skills.
           </p>
 
-          <div className="stats">
+          <div className="about-stats">
 
-            <div className="stat-card">
-              <h2>8+</h2>
+            <div className="stat-box">
+              <h2>4+</h2>
               <span>Projects</span>
             </div>
 
-            <div className="stat-card">
+            <div className="stat-box">
               <h2>1+</h2>
               <span>Hackathons</span>
             </div>
 
-            <div className="stat-card">
+            <div className="stat-box">
               <h2>15+</h2>
               <span>Technologies</span>
             </div>
