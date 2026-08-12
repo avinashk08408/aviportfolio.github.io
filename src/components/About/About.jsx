85cf1dcea1ff5f0d606db9c0958edd850fa66d5e
avinashk08408 <avinashk08408@gmail.com>
2026-08-12 23:47:13 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -34,12 +34,7 @@ function About() {
     problems, and creating solutions that are both <strong>functional and
     secure</strong>.
   </p>
-          <p>
-          <div className="about-quote">
-            <p>"The best way out is always through."</p>
-              <span>--Robert Frost</span>
-            </div>
-          </p>
+          
           
           <div className="about-stats">
 
@@ -58,7 +53,12 @@ function About() {
               <span>Technologies</span>
             </div>
 
-  
+          <p>
+          <div className="about-quote">
+            <p>"The best way out is always through."</p>
+              <span>--Robert Frost</span>
+            </div>
+          </p>
 
 
           </div>
